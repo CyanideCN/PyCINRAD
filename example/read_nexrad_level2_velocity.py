49d528dfc6efcee3aae66b8fc30351254ed65a44
CyanideCN <274555447@qq.com>
2018-10-12 10:56:34 +0800

    Small change

--- example/read_nexrad_level2_velocity.py
+++ example/read_nexrad_level2_velocity.py
@@ -2,7 +2,8 @@
 # Author: Du puyuan
 
 import numpy as np
-from metpy.io.nexrad import Level2File
+import pyart
+import datetime
 from tkinter import filedialog
 
 from cinrad.datastruct import V
@@ -11,25 +12,25 @@ from cinrad.visualize import ppi
 from cinrad.constants import deg2rad
 
 name = filedialog.askopenfilename()
-f = Level2File(name)
-# Pull data out of the file
+radar = pyart.io.read_nexrad_archive(name)
 sweep = 1
-range = 230
-# First item in ray is header, which has azimuth angle
-az = np.array([ray[0].az_angle for ray in f.sweeps[sweep]])
-el = f.sweeps[sweep][0][0].el_angle
-# 5th item is a dict mapping a var name (byte string) to a tuple
-# of (header, data array)
-v_hdr = f.sweeps[sweep][0][4][b'VEL'][0]
-reso = v_hdr.gate_width
-v_range = np.arange(v_hdr.num_gates) * reso + v_hdr.first_gate
-v = np.array([ray[4][b'VEL'][1] for ray in f.sweeps[sweep]])
-slon = f.sweeps[0][0][1].lon
-slat = f.sweeps[0][0][1].lat
-lon, lat = get_coordinate(v_range[:int(range / reso)], az * deg2rad, el, slon, slat)
-timestr = f.dt.strftime('%Y%m%d%H%M%S')
-data = v.T[:int(range / reso)].T
-v_obj = V(data, range, el, reso, f.stid.decode(), f.stid.decode()
-          , timestr, slon, slat, include_rf=False)
+drange = 230
+irange = radar.get_start_end(sweep)
+cali = pyart.correct.dealias_region_based(radar)
+v = cali['data'][irange[0]:irange[1] + 1]
+az = radar.get_azimuth(sweep) * deg2rad
+az = np.append(az, az[0])
+v_range = radar.range['data'] / 1000
+reso = radar.range['meters_between_gates'] / 1000
+slon, slat = radar.longitude['data'][0], radar.latitude['data'][0]
+scantime = datetime.datetime.strptime(radar.time['units'], 'seconds since %Y-%m-%dT%H:%M:%SZ')
+el = radar.get_elevation(sweep)[0]
+lon, lat = get_coordinate(v_range[:int(drange / reso)], az, el, slon, slat)
+timestr = scantime.strftime('%Y%m%d%H%M%S')
+data = v.T[:int(drange / reso)].T
+data = np.concatenate((data, data[0, None]))
+data_f = np.ma.array(data, mask=(data <= -128))
+rf = np.ma.array(data, mask=(data != -128))
+v_obj = V([data_f, rf], drange, el, reso, 'KEVX', 'KEVX', timestr, slon, slat)
 v_obj.add_geoc(lon, lat, np.zeros(lon.shape))
 ppi.base_velocity(v_obj, coastline=True, lscale=True)
