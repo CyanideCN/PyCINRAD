2c4b1ada457c9c75f7c5f79b3759ac64dfafda49
CyanideCN <274555447@qq.com>
2018-10-29 20:17:26 +0800

    Update: Combined plot

--- cinrad/projection.py
+++ cinrad/projection.py
@@ -33,7 +33,7 @@ def get_coordinate(distance, azimuth, elevation, centerlon, centerlat, h_offset=
     distance: int or float or numpy.ndarray
         distance in kilometer in terms of polar coordinate
     azimuth: int or float or numpy.ndarray
-        azimuth in degree in terms of polar coordinate
+        azimuth in radian in terms of polar coordinate
     elevation: int or float
         elevation angle in degree
     centerlon: int or float
@@ -49,8 +49,12 @@ def get_coordinate(distance, azimuth, elevation, centerlon, centerlat, h_offset=
         latitude value
     '''
     elev = elevation if h_offset else 0
-    deltav = np.cos(azimuth[:, np.newaxis]) * distance * np.cos(elev * deg2rad)
-    deltah = np.sin(azimuth[:, np.newaxis]) * distance * np.cos(elev * deg2rad)
+    if isinstance(azimuth, np.ndarray):
+        deltav = np.cos(azimuth[:, np.newaxis]) * distance * np.cos(elev * deg2rad)
+        deltah = np.sin(azimuth[:, np.newaxis]) * distance * np.cos(elev * deg2rad)
+    else:
+        deltav = np.cos(azimuth) * distance * np.cos(elev * deg2rad)
+        deltah = np.sin(azimuth) * distance * np.cos(elev * deg2rad)
     deltalat = deltav / 111
     actuallat = deltalat + centerlat
     deltalon = deltah / 111
--- cinrad/visualize/rhi.py
+++ cinrad/visualize/rhi.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Author: Du puyuan
+# Author: Puyuan Du
 
 from ..constants import rhi_cmap_smooth, norm1, font2
 
