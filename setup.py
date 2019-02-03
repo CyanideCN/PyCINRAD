fe9d97d7a387dacb42e1261dc4ccb41fc61a4c1a
CyanideCN <274555447@qq.com>
2019-02-03 22:19:41 +0800

    Further change based on #28

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     platforms = 'Windows',
     python_requires='>=3.5',
     install_requires = ['metpy>=0.8', 'cartopy>=0.15', 'pyshp==1.2.12', 'pyresample', 'matplotlib>=2.2'],
-    data_files=[('cinrad', ['RadarStation.npy']),
+    data_files=[('cinrad', ['RadarStation.npy', 'china_coordinates.csv']),
                 ('cinrad' + os.path.sep + 'colormap', glob.glob(r'colormap/*.cmap')),
                 ('cinrad' + os.path.sep + 'shapefile', glob.glob(r'shapefile/*'))],
     scripts = [],
