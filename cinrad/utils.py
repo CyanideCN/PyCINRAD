29b3d8a8cd9e7766aae9768f334ccdabefb2727e
CyanideCN <274555447@qq.com>
2019-03-18 19:04:49 +0800

    KDP calculation extracted from wradarlib

--- cinrad/utils.py
+++ cinrad/utils.py
@@ -135,3 +135,131 @@ def potential_maximum_gust_from_reflectivity(ref, distance, elev):
     et = echo_top(ref, distance, elev, 0)
     vil = vert_integrated_liquid(ref, distance, elev)
     return potential_maximum_gust(et, vil)
+
+def lanczos_differentiator(winlen):
+    # Copyright (c) 2011-2018, wradlib developers.
+    m = (winlen - 1) / 2
+    denom = m * (m + 1.) * (2 * m + 1.)
+    k = np.arange(1, m + 1)
+    f = 3 * k / denom
+    return np.r_[f[::-1], [0], -f]
+
+def kdp_from_phidp(phidp, winlen=7, dr=1., method=None):
+    from scipy.interpolate import interp1d
+    from scipy.stats import linregress
+    from scipy.ndimage.filters import convolve1d
+    # Copyright (c) 2011-2018, wradlib developers.
+    """Retrieves :math:`K_{DP}` from :math:`Phi_{DP}`.
+    In normal operation the method uses convolution to estimate :math:`K_{DP}`
+    (the derivative of :math:`Phi_{DP}` with Low-noise Lanczos differentiators.
+    The results are very similar to the fallback moving window linear
+    regression (`method=slow`), but the former is *much* faster, depending on
+    the percentage of NaN values in the beam, though.
+    For further reading please see `Differentiation by integration using \
+    orthogonal polynomials, a survey <https://arxiv.org/pdf/1102.5219>`_ \
+    and `Low-noise Lanczos differentiators \
+    <http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/\
+    lanczos-low-noise-differentiators/>`_.
+    The fast method provides fast :math:`K_{DP}` retrieval but will return NaNs
+    in case at least one value in the moving window is NaN. The remaining gates
+    are treated by using local linear regression where possible.
+    Please note that the moving window size ``winlen`` is specified as the number of
+    range gates. Thus, this argument might need adjustment in case the
+    range resolution changes.
+    In the original publication (:cite:`Vulpiani2012`), the value ``winlen=7``
+    was chosen for a range resolution of 1km.
+    Warning
+    -------
+    The function is designed for speed by allowing to process
+    multiple dimensions in one step. For this purpose, the RANGE dimension
+    needs to be the LAST dimension of the input array.
+    Parameters
+    ----------
+    phidp : :class:`numpy:numpy.ndarray`
+        multi-dimensional array, note that the range dimension must be the
+        last dimension of the input array.
+    winlen : int
+        Width of the window (as number of range gates)
+    dr : float
+        gate length in km
+    method : str
+        If None uses fast convolution based differentiation, if 'slow' uses
+        linear regression.
+    Examples
+    --------
+    >>> import wradlib
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as pl
+    >>> pl.interactive(True)
+    >>> kdp_true = np.sin(3 * np.arange(0, 10, 0.1))
+    >>> phidp_true = np.cumsum(kdp_true)
+    >>> phidp_raw = phidp_true + np.random.uniform(-1, 1, len(phidp_true))
+    >>> gaps = np.concatenate([range(10, 20), range(30, 40), range(60, 80)])
+    >>> phidp_raw[gaps] = np.nan
+    >>> kdp_re = wradlib.dp.kdp_from_phidp(phidp_raw)
+    >>> line1 = pl.plot(np.ma.masked_invalid(phidp_true), "b--", label="phidp_true")  # noqa
+    >>> line2 = pl.plot(np.ma.masked_invalid(phidp_raw), "b-", label="phidp_raw")  # noqa
+    >>> line3 = pl.plot(kdp_true, "g-", label="kdp_true")
+    >>> line4 = pl.plot(np.ma.masked_invalid(kdp_re), "r-", label="kdp_reconstructed")  # noqa
+    >>> lgnd = pl.legend(("phidp_true", "phidp_raw", "kdp_true", "kdp_reconstructed"))  # noqa
+    >>> pl.show()
+    """
+    assert (winlen % 2) == 1, \
+        "Window size N for function kdp_from_phidp must be an odd number."
+
+    shape = phidp.shape
+    phidp = phidp.reshape((-1, shape[-1]))
+
+    # Make really sure winlen is an integer
+    winlen = int(winlen)
+
+    if method == 'slow':
+        kdp = np.zeros(phidp.shape) * np.nan
+    else:
+        window = lanczos_differentiator(winlen)
+        kdp = convolve1d(phidp, window, axis=1)
+
+    # find remaining NaN values with valid neighbours
+    invalidkdp = np.isnan(kdp)
+    if not np.any(invalidkdp.ravel()):
+        # No NaN? Return KdP
+        return kdp.reshape(shape) / 2. / dr
+
+    # Otherwise continue
+    x = np.arange(phidp.shape[-1])
+    valids = ~np.isnan(phidp)
+    kernel = np.ones(winlen, dtype="i4")
+    # and do the slow moving window linear regression
+    for beam in range(len(phidp)):
+        # number of valid neighbours around one gate
+        nvalid = np.convolve(valids[beam], kernel, "same") > winlen / 2
+        # find those gates which have invalid Kdp AND enough valid neighbours
+        nangates = np.where(invalidkdp[beam] & nvalid)[0]
+        # now iterate over those
+        for r in nangates:
+            ix = np.arange(max(0, r - int(winlen / 2)),
+                           min(r + int(winlen / 2) + 1, shape[-1]))
+            # check again (just to make sure...)
+            if np.sum(valids[beam, ix]) < winlen / 2:
+                # not enough valid values inside our window
+                continue
+            kdp[beam, r] = linregress(x[ix][valids[beam, ix]],
+                                      phidp[beam, ix[valids[beam, ix]]])[0]
+        # take care of the start and end of the beam
+        #   start
+        ix = np.arange(0, winlen)
+        if np.sum(valids[beam, ix]) >= 2:
+            kdp[beam, 0:int(winlen / 2)] = linregress(x[ix][valids[beam, ix]],
+                                                      phidp[beam,
+                                                            ix[valids[beam,
+                                                                      ix]]])[0]
+        # end
+        ix = np.arange(shape[-1] - winlen, shape[-1])
+        if np.sum(valids[beam, ix]) >= 2:
+            kdp[beam, -int(winlen / 2):] = linregress(x[ix][valids[beam, ix]],
+                                                      phidp[beam,
+                                                            ix[valids[beam,
+                                                                      ix]]])[0]
+
+    # accounting for forward/backward propagation AND gate length
+    return kdp.reshape(shape) / 2. / dr
