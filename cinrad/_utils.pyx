2466e09ac83dbbe38496b7afed6dc9512094568e
CyanideCN <274555447@qq.com>
2019-06-25 18:53:46 +0800

    Optimize cython codes

--- cinrad/_utils.pyx
+++ cinrad/_utils.pyx
@@ -2,31 +2,31 @@
 cimport numpy as np
 import numpy as np
 cimport cython
+from libc.math cimport sin
 
 cdef double deg2rad, vil_const
-cdef int rm
+cdef int rm = 8500
 
 deg2rad = 3.141592653589793 / 180
-rm = 8500
 vil_const = 3.44e-6
 
 cdef height(np.ndarray distance, double elevation, double radarheight):
     # unit is meter
-    return distance * np.sin(elevation * deg2rad) + distance ** 2 / (2 * rm) + radarheight
+    return distance * sin(elevation * deg2rad) + distance ** 2 / (2 * rm) + radarheight
 
 cdef height_single(double distance, double elevation):
-    return distance * np.sin(elevation * deg2rad) + distance ** 2 / (2 * rm)
+    return distance * sin(elevation * deg2rad) + distance ** 2 / (2 * rm)
 
 @cython.boundscheck(False)
 def vert_integrated_liquid(double[:, :, ::1] ref, double[:, ::1] distance, double[::1] elev,
                            beam_width=0.99, threshold=18., density=False):
-    cdef double v_beam_width, m1, mb, mt, factor, ht, dist, r_tmp
-    cdef np.ndarray hi_arr, VIL, position
+    cdef double v_beam_width, m1, mb, mt, factor, ht, dist, r_tmp, h_higher, h_lower
+    cdef long long[::1] position
     cdef Py_ssize_t xshape, yshape, zshape
     cdef int pos_s, pos_e, l
     v_beam_width = beam_width * deg2rad
     zshape, xshape, yshape = ref.shape[0], ref.shape[1], ref.shape[2]
-    VIL = np.zeros((xshape, yshape))
+    cdef double[:, ::1] VIL = np.zeros((xshape, yshape))
     for i in range(xshape):
         for j in range(yshape):
             vert_r = list()
@@ -36,15 +36,15 @@ def vert_integrated_liquid(double[:, :, ::1] ref, double[:, ::1] distance, doubl
                 vert_r.append(r_tmp)
                 vert_z.append(10 ** (r_tmp / 10))
             dist = distance[i][j] * 1000
-            hi = dist * np.sin(v_beam_width / 2)
-            position = np.where(np.asarray(vert_r) > threshold)[0]
+            hi = dist * sin(v_beam_width / 2)
+            position = np.nonzero(np.asarray(vert_r) > threshold)[0]
             if position.shape[0] == 0:
                 continue
             pos_s = position[0]
             pos_e = position[-1]
             m1 = 0.
-            for l in position[:-1].astype(int):
-                ht = dist * (np.sin(elev[l + 1] * deg2rad) - np.sin(elev[l] * deg2rad))
+            for l in position[:-1]:
+                ht = dist * (sin(elev[l + 1] * deg2rad) - sin(elev[l] * deg2rad))
                 factor = ((vert_z[l] + vert_z[l + 1]) / 2) ** (4 / 7)
                 m1 += vil_const * factor * ht
             if density == False:
@@ -59,12 +59,12 @@ def vert_integrated_liquid(double[:, :, ::1] ref, double[:, ::1] distance, doubl
 
 @cython.boundscheck(False)
 def echo_top(double[:, :, :] ref, double[:, :] distance, double[:] elev, double radarheight, threshold=18.):
-    cdef np.ndarray r, et, h, vert_h, vert_r
+    cdef np.ndarray r, h, vert_h, vert_r
     cdef int xshape, yshape, pos
     cdef list h_
     cdef double z1, z2, h1, h2, w1, w2
     xshape, yshape = ref.shape[1], ref.shape[2]
-    et = np.zeros((xshape, yshape))
+    cdef double[:, ::1] et = np.zeros((xshape, yshape))
     h_ = list()
     for i in elev:
         h = height(np.asarray(distance), i, radarheight)
@@ -82,7 +82,7 @@ def echo_top(double[:, :, :] ref, double[:, :] distance, double[:] elev, double 
                 et[i][j] = vert_h[-1]
                 continue
             else:
-                position = np.where(vert_r >= threshold)[0]
+                position = np.nonzero(vert_r >= threshold)[0]
                 if position[-1] == 0:
                     et[i][j] = vert_h[0]
                     continue
