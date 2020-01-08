af7908c7579ad769bc83011015d737f5bcee870f
CyanideCN <dpy27455447@gmail.com>
2020-01-07 22:50:59 -0800

    Update

--- cinrad/grid.py
+++ cinrad/grid.py
@@ -79,7 +79,7 @@ def grid_2d(
     y: np.ndarray,
     x_out: Optional[np.ndarray] = None,
     y_out: Optional[np.ndarray] = None,
-    resolution: Optional[tuple] = None,
+    resolution: tuple = (1000, 1000),
 ) -> tuple:
     r"""
     Interpolate data in polar coordinates into geographic coordinates
