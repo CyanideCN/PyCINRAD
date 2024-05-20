6263b210f877f35ca2dd10bd5e6c3b244e8d4b80
pysoer <413188893@qq.com>
2024-05-20 13:40:30 +0800

    update: read_auto support PUP

--- cinrad/io/__init__.py
+++ cinrad/io/__init__.py
@@ -4,6 +4,11 @@
 from cinrad.io.base import RadarBase, prepare_file
 from cinrad.io.level2 import *
 from cinrad.io.level3 import *
+from typing import Union
+
+RadarBase_T = Union[
+    StandardData, CinradReader, StandardPUP, MocMosaic, SWAN, PhasedArrayData, PUP
+]
 
 
 def read_level2(filename: str) -> RadarBase:
@@ -16,7 +21,10 @@ def read_level2(filename: str) -> RadarBase:
         return CinradReader(filename)
 
 
-def read_auto(filename: str) -> RadarBase:
+def read_auto(
+    filename: str,
+    **kwargs
+) -> RadarBase_T:
     """Read radar data, auto detected type of file .
     Args:
         filename: file name of radar data.
@@ -29,11 +37,11 @@ def read_auto(filename: str) -> RadarBase:
         4. SWAN, SWAN format data.
         5. CinradReader, cinrad format based data.
         6. PhasedArrayData, standard format phased array radar data(XAD-2023).
-        TODO:PUP & CinradReader(fix later)
+        7: PUP, NEXRAD Level 3 (NIDS) product files.
     """
     with prepare_file(filename) as file:
         flag = file.read(125)
-    if flag[0:4] == b"RSTM":
+    if flag[:4] == b"RSTM":
         if flag[8:12] == b"\x01\x00\x00\x00":
             return StandardData(filename)
         elif flag[8:12] == b"\x02\x00\x00\x00":
@@ -42,12 +50,14 @@ def read_auto(filename: str) -> RadarBase:
             return PhasedArrayData(filename)
         else:
             raise Exception("Unknown standard radar type")
-    elif flag[0:3] == b"MOC":
+    elif flag[:3] == b"MOC":
         return MocMosaic(filename)
-    if flag[50:54] == b"SWAN":
-        return SWAN(filename)
+    elif flag[50:54] == b"SWAN":
+        return SWAN(filename, **kwargs)
+    elif flag[:4] == b"NXUS" or flag[:4] == b"SDUS" or flag[:4] == b"NOUS":
+        return PUP(filename)
     sc_flag = flag[100:106]
     cc_flag = flag[116:122]
     if flag[14:16] == b"\x01\x00" or sc_flag == b"CINRAD" or cc_flag == b"CINRAD":
-        return CinradReader(filename)
+        return CinradReader(filename, **kwargs)
     raise Exception("Unknown radar type")
