38900d459de59e2f397f2b65373df0f1d34e0e86
CyanideCN <dpy274555447@gmail.com>
2024-07-31 23:29:56 -0700

    Bump version to 1.9.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ data_pth = join("cinrad", "data")
 
 setup(
     name="cinrad",
-    version="1.9.0",
+    version="1.9.1",
     description="Decode CINRAD radar data and visualize",
     long_description="Decode CINRAD radar data and visualize",
     license="GPL Licence",
