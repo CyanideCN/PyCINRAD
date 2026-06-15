7adea9982c3148bbaab7169f601f90754696f575
pysoer <413188893@qq.com>
2026-06-15 20:47:41 +0800

    fix: handle missing Arial font on Linux

--- cinrad/visualize/__init__.py
+++ cinrad/visualize/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author: PyCINRAD Developers
 
-import matplotlib
 
-if "Arial" in matplotlib.rc_params()["font.sans-serif"]:
-    matplotlib.rc("font", family="Arial")
 from cinrad.visualize.ppi import *
 from cinrad.visualize.rhi import *
